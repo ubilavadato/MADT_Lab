d1d14011b25524c1cf2389e243c82ce518e986b0
ubilavadato <ubilavadato5@gmail.com>
2025-10-01 22:28:37 +0300

    Add second Button; change TextView color on click

--- app/src/main/java/com/example/madt_lab/MainActivity.kt
+++ app/src/main/java/com/example/madt_lab/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.madt_lab
+package com.example.madt_lab   // <-- change this if your package is different
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -10,17 +11,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var tvMessage: TextView
     private lateinit var btnChangeText: Button
+    private lateinit var btnChangeColor: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        // connect views from XML
         tvMessage = findViewById(R.id.tvMessage)
         btnChangeText = findViewById(R.id.btnChangeText)
+        btnChangeColor = findViewById(R.id.btnChangeColor)
 
+        // Button 1: change the text
         btnChangeText.setOnClickListener {
             tvMessage.text = "Hello from MADT Lab 1"
+        }
+
+        // Button 2: change text color
+        btnChangeColor.setOnClickListener {
+            tvMessage.setTextColor(Color.RED)
         }
     }
 }
